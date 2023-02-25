b2a105f67d4b7e32b81ee9a5889e527ec54567e8
besyia0k0 <besyia0k0@gmail.com>
2023-02-25 15:42:35 +0900

    Fix: main문 다듬기

--- module-01/ex00/main.cpp
+++ module-01/ex00/main.cpp
@@ -6,17 +6,32 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 22:02:05 by hkong             #+#    #+#             */
-/*   Updated: 2023/02/24 22:19:25 by hkong            ###   ########.fr       */
+/*   Updated: 2023/02/25 15:41:02 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main(void) {
-	Zombie	*stackZombie = newZombie("stack");
+	/* default stack zombie created */
+	Zombie	defaultStackZombie;
 
-	stackZombie->announce();
-	randomChump("heap");
-	delete stackZombie;
+	defaultStackZombie.announce();
+	std::cout << std::endl;
+	/* named stack zombie created */
+	Zombie	namedStackZombie("named_stack");
+
+	namedStackZombie.announce();
+	std::cout << std::endl;
+	/* heap zombie created */
+	Zombie	*heapZombie = newZombie("heap");
+
+	heapZombie->announce();
+	std::cout << std::endl;
+	/* stack zombie created & deleted in function*/
+	randomChump("stack");
+	std::cout << std::endl;
+
+	delete heapZombie;
 	return (0);
 }
