845f23eea0e204a11c1a2edceaaa1dc604e2f82b
besyia0k0 <besyia0k0@gmail.com>
2023-02-27 22:05:35 +0900

    Feature: module02 ex00 fixed class base

--- module-02/ex00/Fixed.hpp
+++ module-02/ex00/Fixed.hpp
@@ -6,7 +6,28 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:30:52 by hkong             #+#    #+#             */
-/*   Updated: 2023/02/27 16:30:53 by hkong            ###   ########.fr       */
+/*   Updated: 2023/02/27 16:36:06 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+	private:
+		int	value;
+		static const int	fractional_bit = 8;
+	
+	public:
+		Fixed();
+		Fixed(const Fixed &fixed);
+		~Fixed();
+		Fixed &operator= (const Fixed &fixed);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+#endif
