c60bb7bd65f3a9c42f2a5f11443b4d428c196b67
Hyeonsol Kong <besyia0k0@gmail.com>
2023-03-09 15:13:20 +0900

    Feature: module02 ex03 base

--- module-02/ex03/Point.cpp
+++ module-02/ex03/Point.cpp
@@ -6,8 +6,22 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 20:12:39 by hkong             #+#    #+#             */
-/*   Updated: 2023/03/03 20:13:08 by hkong            ###   ########.fr       */
+/*   Updated: 2023/03/05 00:16:41 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+
+Point::Point(): x(0), y(0) {}
+
+Point::Point(const float x_, const float y_): x(x_), y(y_){}
+
+Point::Point(const Point& point) {
+	x = point.x;
+	y = point.y;
+}
+
+Point& Point::operator=(const Point& point) {
+	
+	return *this;
+}
--- module-02/ex03/Point.hpp
+++ module-02/ex03/Point.hpp
@@ -6,11 +6,25 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 20:12:41 by hkong             #+#    #+#             */
-/*   Updated: 2023/03/03 20:13:02 by hkong            ###   ########.fr       */
+/*   Updated: 2023/03/05 00:12:11 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef POINT_HPP
 # define POINT_HPP
+
+# include "Fixed.hpp"
+class Point {
+	private:
+		Fixed	x;
+		Fixed	y;
+		
+	public:
+		Point();
+		Point(const float x_, const float y_);
+		Point(const Point& point);
+		Point& operator=(const Point& point);
+		~Point();
+};
 
 #endif
