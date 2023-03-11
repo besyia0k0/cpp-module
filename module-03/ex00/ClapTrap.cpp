ec74fa10d2a486e0cb4db6fec3c561dd13b843ba
Hyeonsol Kong <besyia0k0@gmail.com>
2023-03-11 14:42:01 +0900

    Feature: module03 ex00 writing

--- module-03/ex00/ClapTrap.cpp
+++ module-03/ex00/ClapTrap.cpp
@@ -6,7 +6,43 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 22:05:55 by hkong             #+#    #+#             */
-/*   Updated: 2023/03/09 22:05:57 by hkong            ###   ########.fr       */
+/*   Updated: 2023/03/10 01:11:47 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ClapTrap.hpp>
+
+ClapTrap::ClapTrap(std::string name_): name(name_), hit_points(10), energy_points(10), attack_damage(0) {
+	std::cout << "+ Default constructor called +" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& clapTrap) {
+	std::cout << "+ Copy constructor called +" << std::endl;
+	*this = clapTrap;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &clapTrap) {
+	std::cout << "= Copy assignment operator called =" << std::endl;
+	name = clapTrap.name;
+	hit_points = clapTrap.hit_points;
+	energy_points = clapTrap.energy_points;
+	attack_damage = clapTrap.attack_damage;
+	return *this;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "- Destructor called -" << std::endl;
+}
+
+void	ClapTrap::attack(const std::string& target) {
+	std::cout << "⚔️ ClapTrap " << name << " attacks " << target << \
+		", causing " << attack_damage << " points of damage! ⚔️" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	std::cout << "😵 ClapTrap " << name << " takes " << amount << " damage. 😵" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	std::cout << "🩹 ClapTrap " << name << " repairs itself, it gets " << amount << " hit point back! 🩹" << std::endl;
+}
--- module-03/ex00/ClapTrap.hpp
+++ module-03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 22:05:51 by hkong             #+#    #+#             */
-/*   Updated: 2023/03/09 22:08:10 by hkong            ###   ########.fr       */
+/*   Updated: 2023/03/10 00:55:28 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 # define CLAPTRAP_HPP
 
 # include <iostream>
+# include <string>
 
 class ClapTrap {
 	private:
+		std::string name;
+		int					hit_points;
+		int					energy_points;
+		int					attack_damage;
 
 	public:
-		ClapTrap();
+		ClapTrap(std::string name_);
 		ClapTrap(const ClapTrap& clapTrap);
 		ClapTrap& operator=(const ClapTrap &clapTrap);
 		~ClapTrap();
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
 };
 
 
