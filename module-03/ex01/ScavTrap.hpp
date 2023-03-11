0af9e22748f3aaf7b8142b4eac4a38cefad7bab0
besyia0k0 <besyia0k0@gmail.com>
2023-03-11 21:12:31 +0900

    Feature: module03 ex01 fin

---
+++ module-03/ex01/ScavTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 18:19:44 by hkong             #+#    #+#             */
+/*   Updated: 2023/03/11 21:06:20 by hkong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <ClapTrap.hpp>
+# define BLUE "\x1B[34m"
+
+class ScavTrap: public ClapTrap {
+	private:
+
+	public:
+		ScavTrap();
+		ScavTrap(std::string name_);
+		ScavTrap(const ScavTrap& scavTrap);
+		ScavTrap& operator=(const ScavTrap& scavtrap);
+		~ScavTrap();
+		
+		void	attack(const std::string& target);
+		void	guardGate();
+};
+
+#endif
