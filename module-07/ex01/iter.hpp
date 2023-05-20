298f101f7fa494f2627a2e54aa14ade435536547
Hyeonsol Kong <besyia0k0@gmail.com>
2023-05-20 11:53:56 +0900

    Feature: module07 ex01 fin

--- module-07/ex01/iter.hpp
+++ module-07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 22:18:49 by hkong             #+#    #+#             */
-/*   Updated: 2023/05/18 22:26:45 by hkong            ###   ########.fr       */
+/*   Updated: 2023/05/20 11:52:57 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,33 @@
 # define ITER_HPP
 # include <iostream>
 
+# define YELLOW "\x1B[33m"
+# define GREEN "\x1B[32m"
+# define RED "\x1B[31m"
+# define CLOSE "\x1B[0m"
+
 template <typename T>
-void iter(const T (&array)[], size_t size, T (*func)(T&)) {
+void iter(T* array, size_t size, void (*func)(T&)) {
 	for (size_t i = 0; i < size; i++) {
 		func(array[i]);
 	}
+}
+
+template <typename T>
+void iter(const T* array, size_t size, void (*func)(const T&)) {
+	for (size_t i = 0; i < size; i++) {
+		func(array[i]);
+	}
+}
+
+template <typename T>
+void printAll(T& element) {
+	std::cout << "[ " << element << " ]" << std::endl;
+}
+
+template <typename T>
+void increase(T& element) {
+	element++;
 }
 
 #endif
--- module-07/ex01/main.cpp
+++ module-07/ex01/main.cpp
@@ -6,12 +6,33 @@
 /*   By: hkong <hkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 22:18:51 by hkong             #+#    #+#             */
-/*   Updated: 2023/05/18 22:27:42 by hkong            ###   ########.fr       */
+/*   Updated: 2023/05/20 11:53:23 by hkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
 int main(void) {
-	
+	std::cout << YELLOW << "----------- Test 1 -----------" << CLOSE << std::endl;
+	{
+		int size = 5;
+		int array[size];
+
+		for (int i = 0; i < size; i++) {
+			array[i] = i + 1;
+		}
+		std::cout << "----------- before -----------" << std::endl;
+		::iter(array, size, printAll);
+		::iter(array, size, increase);
+		std::cout << "----------- after -----------" << std::endl;
+		::iter(array, size, printAll);
+	}
+	std::cout << YELLOW << "----------- Test 2 -----------" << CLOSE << std::endl;
+	{
+		int size = 5;
+		const std::string array[5] = {"hello", "my", "name", "is", "hkong"};
+
+		::iter(array, size, printAll);
+	}
+	std::cout << YELLOW << "----------- Test Fin -----------" << CLOSE << std::endl;
 }
